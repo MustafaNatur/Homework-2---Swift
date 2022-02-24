9b7b07c7f3d48998def6f6781af3afdbee05b608
MustafaNatur <mustafa.natur@mail.ru>
2022-02-24 21:34:21 +0300

    Initial Commit

---
+++ Home work 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Home work 2
+//
+//  Created by Мустафа Натур on 24.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
