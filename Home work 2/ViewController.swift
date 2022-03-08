33766eb48916232345fdb364d1720769de01d0a8
MustafaNatur <mustafa.natur@mail.ru>
2022-03-08 15:24:58 +0300

    added 2 functions to simplify the code

--- Home work 2/ViewController.swift
+++ Home work 2/ViewController.swift
@@ -14,18 +14,9 @@ class ViewController: UIViewController {
         
         let const = ( sliderValueRed.maximumValue - sliderValueRed.minimumValue)/2
         
-        sliderValueRed.value = const
+        setConstValue(value: const)
         
-        slierValueGreen.value = const
-        
-        slierValueBlue.value = const
-        
-        labelValueSliderRed.text = "\(Int(const))"
-        
-        labelValueSliderGreen.text = "\(Int(const))"
-
-        labelValueSliderBlue.text = "\(Int(const))"
-
+        setcolor()
         
         ViewForColor.backgroundColor = UIColor(red: CGFloat(const)/255, green: CGFloat(const)/255, blue: CGFloat(const)/255, alpha: 1.0)
         
@@ -36,74 +27,60 @@ class ViewController: UIViewController {
 
 
     @IBOutlet weak var labelValueSliderRed: UILabel!
-    
     @IBOutlet weak var labelValueSliderGreen: UILabel!
-    
     @IBOutlet weak var labelValueSliderBlue: UILabel!
-    
     @IBOutlet weak var ViewForColor: UIView!
-    
-    
     @IBOutlet weak var sliderValueRed: UISlider!
-    
-    @IBOutlet weak var slierValueGreen: UISlider!
-    
-    @IBOutlet weak var slierValueBlue: UISlider!
+    @IBOutlet weak var sliderValueGreen: UISlider!
+    @IBOutlet weak var sliderValueBlue: UISlider!
     
     
-    
-    @IBAction func sliderRed(_ sender: Any) {
-        labelValueSliderRed.text = "\(Int(sliderValueRed.value))"
-        change1lslider()
-    }
-    
-    
-    @IBAction func sliderGreen(_ sender: Any) {
-        labelValueSliderGreen.text = "\(Int(slierValueGreen.value))"
-        change1lslider()
-    }
-    
-    @IBAction func sliderBlue(_ sender: Any) {
-        labelValueSliderBlue.text = "\(Int(slierValueBlue.value))"
-        change1lslider()
-    }
-    
-    func change1lslider(){
-        ViewForColor.backgroundColor = UIColor(red: CGFloat(sliderValueRed.value)/255, green: CGFloat(slierValueGreen.value)/255, blue: CGFloat(slierValueBlue.value)/255, alpha: 1.0)
-    }
-    
-    
-    @IBAction func ButtonMin(_ sender: Any) {
-        sliderValueRed.value = 0
-        slierValueGreen.value = 0
-        slierValueBlue.value = 0
-        setcolor()
-    }
-    
-    
-    @IBAction func ButtonMedium(_ sender: Any) {
-        sliderValueRed.value = 127
-        slierValueGreen.value = 127
-        slierValueBlue.value = 127
-        setcolor()
-    }
     
     func setcolor(){
         labelValueSliderRed.text = "\(Int(sliderValueRed.value))"
-        labelValueSliderGreen.text = "\(Int(slierValueGreen.value))"
-        labelValueSliderBlue.text = "\(Int(slierValueBlue.value))"
-        change1lslider()
+        labelValueSliderGreen.text = "\(Int(sliderValueGreen.value))"
+        labelValueSliderBlue.text = "\(Int(sliderValueBlue.value))"
+        changeViewValue()
     }
     
-    @IBAction func ButtonMin1(_ sender: Any) {
-        
+    func changeViewValue(){
+        ViewForColor.backgroundColor = UIColor(red: CGFloat(sliderValueRed.value)/255, green: CGFloat(sliderValueGreen.value)/255, blue: CGFloat(sliderValueBlue.value)/255, alpha: 1.0)
     }
-    @IBAction func ButtonMax(_ sender: Any) {
-        sliderValueRed.value = 255
-        slierValueGreen.value = 255
-        slierValueBlue.value = 255
+    
+    func setConstValue(value: Float){
+        sliderValueRed.value = value
+        sliderValueGreen.value = value
+        sliderValueBlue.value = value
+    }
+    
+    @IBAction func sliderRed(_ sender: Any) {
+        labelValueSliderRed.text = "\(Int(sliderValueRed.value))"
+        changeViewValue()
+    }
+    
+    @IBAction func sliderGreen(_ sender: Any) {
+        labelValueSliderGreen.text = "\(Int(sliderValueGreen.value))"
+        changeViewValue()
+    }
+    
+    @IBAction func sliderBlue(_ sender: Any) {
+        labelValueSliderBlue.text = "\(Int(sliderValueBlue.value))"
+        changeViewValue()
+    }
+    
+    @IBAction func ButtonMin(_ sender: Any) {
+        setConstValue(value: 0)
         setcolor()
-        
+    }
+    
+    @IBAction func ButtonMedium(_ sender: Any) {
+        setConstValue(value: 127)
+        setcolor()
+    }
+    
+    @IBAction func ButtonMax(_ sender: Any) {
+        setConstValue(value: 255)
+        setcolor()
     }
 }
 
