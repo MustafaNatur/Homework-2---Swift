3697da014408da2aaeea87858d957fe9d8599adc
MustafaNatur <mustafa.natur@mail.ru>
2022-03-08 16:16:44 +0300

    Merged 2 functions: setcolor() and  changeViewValue()

--- Home work 2/ViewController.swift
+++ Home work 2/ViewController.swift
@@ -14,7 +14,6 @@ class ViewController: UIViewController {
         let const = ( sliderValueRed.maximumValue - sliderValueRed.minimumValue)/2
         
         setConstValue(value: const) // set const for sliderValue
-        setcolor() // label -> sliderValue
         changeViewValue() // ViewColor -> sliderValue
     }
 
@@ -29,16 +28,10 @@ class ViewController: UIViewController {
     @IBOutlet weak var sliderValueGreen: UISlider!
     @IBOutlet weak var sliderValueBlue: UISlider!
     
-    
-    
-    func setcolor(){
+    func changeViewValue(){
         labelValueSliderRed.text = "\(Int(sliderValueRed.value))"
         labelValueSliderGreen.text = "\(Int(sliderValueGreen.value))"
         labelValueSliderBlue.text = "\(Int(sliderValueBlue.value))"
-        changeViewValue()
-    }
-    
-    func changeViewValue(){
         ViewForColor.backgroundColor = UIColor(red: CGFloat(sliderValueRed.value)/255, green: CGFloat(sliderValueGreen.value)/255, blue: CGFloat(sliderValueBlue.value)/255, alpha: 1.0)
     }
     
@@ -46,6 +39,7 @@ class ViewController: UIViewController {
         sliderValueRed.value = value
         sliderValueGreen.value = value
         sliderValueBlue.value = value
+        changeViewValue()
     }
     
     @IBAction func sliderRed(_ sender: Any) {
@@ -65,17 +59,14 @@ class ViewController: UIViewController {
     
     @IBAction func ButtonMin(_ sender: Any) {
         setConstValue(value: 0)
-        setcolor()
     }
     
     @IBAction func ButtonMedium(_ sender: Any) {
         setConstValue(value: 127)
-        setcolor()
     }
     
     @IBAction func ButtonMax(_ sender: Any) {
         setConstValue(value: 255)
-        setcolor()
     }
 }
 
